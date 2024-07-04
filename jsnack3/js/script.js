932419ab341d53e37d1a730878a9c866af7edba1
Denise Pingiori <pingiorid@gmail.com>
2024-07-04 17:57:58 +0200

    [upd] snack3 finish

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -4,10 +4,20 @@ document.getElementById('send-number').addEventListener('click', function () {
     //recupero valore campo input inserito dall'user
     let num = document.getElementById('number').value; 
     
-    //dichiaro var che contiene somma delle cifre
-    let somma = 0;
+    
+//verifico che il valore inserito sia lungo esattamente 4 cifre
+    if(num.length === 4){
 
-    for (let i = 0; i < num.length; i++) {
+        //dichiaro var che contiene somma delle cifre
+        let somma = 0;
+         for (let i = 0; i < num.length; i++) {
         console.log(num[i]);
+        somma += parseInt(num[i]);
+        
+        }
+        console.log(somma);
+    }
+    else {
+        alert('non hai inserito un valore di 4 cifre, ma inferiore o superiore');
     }
 });
