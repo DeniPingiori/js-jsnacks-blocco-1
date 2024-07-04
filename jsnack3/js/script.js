bd1e4a5a54cbe06f770cda2bf1abacf8308857ee
Denise Pingiori <pingiorid@gmail.com>
2024-07-04 17:46:31 +0200

    [upd] upd for

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,13 @@
-//
+//funzione click
+document.getElementById('send-number').addEventListener('click', function () {
+    
+    //recupero valore campo input inserito dall'user
+    let num = document.getElementById('number').value; 
+    
+    //dichiaro var che contiene somma delle cifre
+    let somma = 0;
+
+    for (let i = 0; i < num.length; i++) {
+        console.log(num[i]);
+    }
+});
