e09c54ed6dc3905cf039722581768aa8e9349233
Denise Pingiori <pingiorid@gmail.com>
2024-07-04 16:49:56 +0200

    [upd] upd if with odd num

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,5 +1,15 @@
 //array vuoto
-let = numbers = [];
+let = numbers = [   ];
 
 //ciclo di 6 iterazioni
+for (let i=0; i<6; i++) {
+    //permetto all'user di inserire numero
+    let num = prompt('inserisci un numero');
+
+    //verifico se il num è dispari
+    if (num % 2 != 0) {
+        //metto nell'array
+        numbers.push(num)
+    }
+}
 
